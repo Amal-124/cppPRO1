07e5cccfa84eab7c486545d4f03fef6d198049d7
A201600321 <A201600321@du.edu.om>
2019-10-22 14:26:03 +0400

    m

--- project1.cpp
+++ project1.cpp
@@ -1,10 +1,16 @@
 #include <iostream> //access control system
 using namespace std;
 // each one has a specific room to access 
-class person { //PARENT CLASS
-	public:
+class person { //BASE CLASS
+	protected:
 			int id; //memebers-attributes
 			string name;
+};			string role;
+class room{ //BASE CLASS 2
+	protected:
+			int id; //memebers-attributes
+			string type;
+			string building;
 };
 class student:public person //declare the inheritance
 	{
@@ -12,29 +18,33 @@ class student:public person //declare the inheritance
 	}; //end
 class faculty:public person //inheriets publicly from person
 	{
-		
+		private:
+		string Speciality;
 	};//end
 class tech:public person //name of class
 	{
-		
+	private:
+		string Speciality;	
 	};//end
 class janitor:public person
 	{
 		
 	};//end
-class FacOffices
+class FacOffices:public room
 	{
-	
+	private:
+		string Faculty;
 	};//end
-class ClRooms
+class ClRooms:public room
 	{
-	
+	private:
+		float capacity;
 	};//end
-class TechRooms
+class TechRooms:public room
 	{
-	
+	private:
+		int hazardLevel;
 	};//end
-
 
 
 
