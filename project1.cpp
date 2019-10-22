f9ad4d41f557592370e4d76b9ec6da5abcd375a4
A201600321 <A201600321@du.edu.om>
2019-10-22 14:58:44 +0400

    m

--- project1.cpp
+++ project1.cpp
@@ -3,12 +3,16 @@ using namespace std;
 // each one has a specific room to access 
 class person { //BASE CLASS
 	protected:
-			int id; //memebers-attributes
+			string id; //memebers-attributes
 			string name;
-};			string role;
+			string role;
+	public:
+			//constructers
+	};
+
 class room{ //BASE CLASS 2
 	protected:
-			int id; //memebers-attributes
+			string id; //memebers-attributes
 			string type;
 			string building;
 };
@@ -20,44 +24,60 @@ class faculty:public person{ //inheriets publicly from person
 		private:
 		 string Speciality;
 		public:
+			//constructer that inheiertes
 			
 	};//end
 class tech:public person { //name of class	
 	private:
 		string Speciality;
 	public:
-		void SetSpeciality(string sp);
+		void SetSpeciality(string SP);
 		string GetSpeciality(void);
 		
 	};//end
+// setter and getter for the class tech
+void tech::SetSpeciality(string SP) {Speciality =SP;}	
+string tech::GetSpeciality(void){ return Speciality; }
+
 class janitor:public person{
 
 		
 	};//end
-class FacOffices:public room{
+class FacOffices:public room{ //constructer
 	private:
 		string Faculty;
 	public:	
-		void Setfaculty(string fa);
+		void Setfaculty(string FA);
 		string Getfaculty(void);
 	
 	};//end
+// setter and getter for the class	
+void FacOffices::Setfaculty(string FA) {Faculty =FA;}	
+string FacOffices::Getfaculty(void){ return Faculty; }	
+
 class ClRooms:public room{
 	private:
 		float capacity;
 	public:	
-		void SetCapacity(string ca);
-		string GetCapacity(void);
+		void SetCapacity(float ca);
+		float GetCapacity(void);
 	
 	};//end
+// setter and getter for the class	
+void ClRooms::SetCapacity( float ca){capacity = ca;}
+float ClRooms::GetCapacity(void){return capacity;}
+
 class TechRooms:public room{
 	private:
 		int hazardLevel;
 	public:
 		void SetHL(string ha);
-		string GetHL(void);	
+		int GetHL(void);	
 	
 	};//end
+// setter and getter for the class	
+void TechRooms::SetHL(string ha){ hazardLevel = ha;}
+string TechRooms::GetHL(void){return hazardLevel;}
 int main()
 {
 		
